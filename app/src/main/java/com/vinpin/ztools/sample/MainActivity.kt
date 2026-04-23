4a3b0777116fa0e5d5b8310d68d460adbb8cb160
VinPin <hearzwp@163.com>
2026-04-23 21:54:34 +0800

    feat(android): 初始化Android项目基础配置和资源文件
    - 添加AndroidManifest.xml配置应用基本信息及启动Activity
    - 配置Gradle构建脚本，指定应用ID、SDK版本和编译选项
    - 增加Android资源文件，包括颜色、图标和主题样式
    - 新建MainActivity实现基础Activity并启用EdgeToEdge模式
    - 添加备份规则与数据提取规则XML文件示例
    - 引入Gradle Wrapper脚本及相关配置文件确保项目构建环境一致
    - 配置项目级Gradle文件及插件版本管理
    - 设置kotlin代码风格和AndroidX开启相关属性
    - 配置IDEA项目文件，包含运行配置及项目迁移设置

---
+++ app/src/main/java/com/vinpin/ztools/sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vinpin.ztools.sample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
