59c7467bffdf16802b9a2d085aee9caec115dd24
VinPin <hearzwp@163.com>
2026-04-27 20:28:26 +0800

    feat(util): 添加快速点击判断工具类
    - 新增 FastClickUtil 对象用于检测快速点击
    - 实现 isFastClick 方法以判断是否在指定间隔内点击
    - 默认点击间隔时间为300毫秒
    - 记录上一次点击时间避免频繁触发事件

---
+++ ztools/src/main/java/com/vinpin/ztools/FastClickUtil.kt
@@ -0,0 +1,23 @@
+package com.vinpin.ztools
+
+/**
+ * 快速点击工具类
+ */
+object FastClickUtil {
+
+    private var lastFastClickTime: Long = 0L
+
+    /**
+     * 判断是不是快速点击
+     *
+     * @param intervalTime 间隔时间，单位毫秒，默认值300毫秒
+     * @return true:是快速点击，false:不是快速点击
+     */
+    fun isFastClick(intervalTime: Long = 300L): Boolean {
+        val t = System.currentTimeMillis()
+        val dt: Long = t - lastFastClickTime
+        if (dt in 1..intervalTime) return true
+        lastFastClickTime = t
+        return false
+    }
+}
