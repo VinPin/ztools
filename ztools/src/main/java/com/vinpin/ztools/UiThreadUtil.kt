aec31ed8d93782fe602cb1fd04ce59617a388318
VinPin <hearzwp@163.com>
2026-04-27 15:39:55 +0800

    feat(utils): 添加与UI线程交互的工具类
    - 创建UiThreadUtil对象封装UI线程相关操作
    - 实现判断当前线程是否为UI线程的方法
    - 支持在UI线程立即执行或延迟执行Runnable
    - 提供移除挂起Runnable的方法
    - 以主线程Handler作为消息处理基础

---
+++ ztools/src/main/java/com/vinpin/ztools/UiThreadUtil.kt
@@ -0,0 +1,42 @@
+package com.vinpin.ztools
+
+import android.os.Handler
+import android.os.Looper
+
+/**
+ * 与UI线程进行交互的实用工具
+ */
+object UiThreadUtil {
+    /**
+     * 主线程的Handler
+     */
+    private val mainHandler by lazy { Handler(Looper.getMainLooper()) }
+
+    /**
+     * 判断当前是否在UI线程
+     */
+    fun isOnUiThread(): Boolean {
+        return Looper.getMainLooper().thread === Thread.currentThread()
+    }
+
+    /**
+     * 在UI线程上运行给定的`Runnable`
+     */
+    fun runOnUiThread(runnable: Runnable) {
+        runOnUiThread(runnable, 0)
+    }
+
+    /**
+     * 以指定的延迟在UI线程上运行给定的`Runnable`
+     */
+    fun runOnUiThread(runnable: Runnable, delayInMs: Long) {
+        mainHandler.postDelayed(runnable, delayInMs)
+    }
+
+    /**
+     * 删除消息队列中所有挂起的给定的`Runnable`
+     */
+    fun removeCallbacks(runnable: Runnable) {
+        mainHandler.removeCallbacks(runnable)
+    }
+}
